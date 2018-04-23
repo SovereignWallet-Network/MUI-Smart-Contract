95ec078b7b03dac37907baff83166ba3399c4c99
rachel <35097890+Rachel920706@users.noreply.github.com>
2018-04-23 18:11:25 +0900

    comment 삭제

--- contracts/MuiTrade.sol
+++ contracts/MuiTrade.sol
@@ -18,7 +18,6 @@ contract MuiTrade is Withdrawable,Utils {
   uint256 public exchangePrice;
   uint256 public exchangeSupply;
   uint256 public availableSupply;
-  //mapping(address=>bool) public isReserve; // @TODO : Will be implemented in the future after figuring what the feature of reserve and how it works
 
   function MuiTrade(ERC20 _muiToken, address _admin, uint256 _sellPrice, uint256 _buyPrice, uint256 _availableSupply) public {
       require(_admin != address(0));
@@ -30,7 +29,6 @@ contract MuiTrade is Withdrawable,Utils {
   }
 
   function() public payable {
-      //require(isReserve[msg.sender]);
       EtherReceival(msg.sender, msg.value);
   }
 
