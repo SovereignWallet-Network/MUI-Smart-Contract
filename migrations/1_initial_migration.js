02fcea6e18133d876ff526e45128dbcd8d02264e
Mustafa Morca <psychoplasma@gmail.com>
2018-06-26 00:19:55 +0900

    Add delays for migrations deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,3 +3,11 @@ var Migrations = artifacts.require("./Migrations.sol");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 };
+
+
+module.exports = (deployer) => {
+    deployer.deploy(Migrations)
+        .then(() => Migrations.deployed())
+        .then(registry => new Promise(resolve => setTimeout(() => resolve(registry), 60000)))
+        .catch(e => console.log(`Deployer failed. ${e}`));
+};
