78567da815d262b072cd951e16cfb65bbdfe2529
Mustafa Morca <psychoplasma@gmail.com>
2018-08-30 10:34:23 +0900

    Add gasPrice option to network configurations for deployment

--- truffle.js
+++ truffle.js
@@ -6,17 +6,26 @@ module.exports = {
     "mainnet": {
       provider: () => new HDWalletProvider(Deployer.mnemonics.mainnet, Deployer.web3Providers.mainnet),
       network_id: 1,
-      gas: 4700000
+      gas: 4700000,
+      gasPrice: 100000000000  // 100 Gwei, Change this value according to price average of the deployment time
     },
     "ropsten": {
       provider: () => new HDWalletProvider(Deployer.mnemonics.testnet, Deployer.web3Providers.ropsten),
       network_id: 3,
-      gas: 4700000
+      gas: 4700000,
+      gasPrice: 50000000000  // 50 Gwei
     },
     "rinkeby": {
       provider: () => new HDWalletProvider(Deployer.mnemonics.testnet, Deployer.web3Providers.rinkeby),
       network_id: 4,
-      gas: 4700000
+      gas: 4700000,
+      gasPrice: 50000000000  // 50 Gwei
+    },
+    "kovan": {
+      provider: () => new HDWalletProvider(Deployer.mnemonics.testnet, Deployer.web3Providers.kovan),
+      network_id: 42,
+      gas: 4700000,
+      gasPrice: 50000000000  // 50 Gwei
     }
   }
 };
